83e4d60c3298f6bc9d173d669d0d9d55f05b868a
ViktorShchekotikhin <vitia046@gmail.com>
2019-03-15 02:17:47 +0200

    Previous portfolio

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-var offset = 80;
+let offset = 20;
 
 $('.navbar li a').click(function(event) {
     event.preventDefault();
     $($(this).attr('href'))[0].scrollIntoView();
     scrollBy(0, -offset);
+});
+
+
+
+$(document).ready(function(){
+    $("body header #scrl").on("click","a",function (event){
+        event.preventDefault();
+        let id = $(this).attr('href'),
+        top = $(id).offset().top();
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
